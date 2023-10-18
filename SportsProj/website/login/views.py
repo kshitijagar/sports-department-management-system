111b511045f9d469959e7b96e97d5b9727bb8ca0
kshitijagar <kshitijagarwal2808@gmail.com>
2023-10-18 10:07:58 +0530

    minor fixes

--- SportsProj/website/login/views.py
+++ SportsProj/website/login/views.py
@@ -1,28 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import mysql.connector as sql
 
-
-em=''
-pwd=''
-
-# Create your views here.
 def loginaction(request):
-    global em,pwd
-    if request.method=="POST":
-        m=sql.connect(host="localhost", user="root", passwd="kshitij2803", database="sports")
-        cursor=m.cursor()
-        d=request.POST
-        for key,value in d.items():
-           
-            if key=="email":
-                em=value
-            if key=="password":
-                pwd=value   
-        c="select * from users where email='{}' and password='{}'".format(em,pwd)
+    global em, pwd
+    error_message = ''  # Initialize error_message here
+
+    if request.method == "POST":
+        m = sql.connect(host="localhost", user="root", passwd="kshitij2803", database="sports")
+        cursor = m.cursor()
+        d = request.POST
+        for key, value in d.items():
+            if key == "email":
+                em = value
+            if key == "password":
+                pwd = value
+
+        c = "select * from users where email='{}' and password='{}'".format(em, pwd)
         cursor.execute(c)
-        t=tuple(cursor.fetchall())
-        if t==():
-            return render(request,'error.html')
-        else:
+        t = tuple(cursor.fetchall())
+        if t != ():
             return render(request, "welcome.html")
-    return render(request, 'login_page.html')
+        else:
+            error_message = "Invalid username or password."
+
+    return render(request, 'login_page.html', {'error_message': error_message})
--- SportsProj/website/website/urls.py
+++ SportsProj/website/website/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('signup/', signaction),
     path('login/' ,loginaction)
+    
 ]
