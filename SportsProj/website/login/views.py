0efb859f411aaa4e8c5ba4e4254a3efd8785a943
kshitijagar <kshitijagarwal2808@gmail.com>
2023-10-18 10:14:02 +0530

    changes in login

--- SportsProj/website/login/views.py
+++ SportsProj/website/login/views.py
@@ -2,18 +2,20 @@ from django.shortcuts import render, redirect
 import mysql.connector as sql
 
 def loginaction(request):
-    global em,pwd
-    if request.method=="POST":
-        m=sql.connect(host="localhost", user="root", passwd="kshitij2803", database="sports")
-        cursor=m.cursor()
-        d=request.POST
-        for key,value in d.items():
-           
-            if key=="email":
-                em=value
-            if key=="password":
-                pwd=value   
-        c="select * from users where email='{}' and password='{}'".format(em,pwd)
+    global em, pwd
+    error_message = ''  # Initialize error_message here
+
+    if request.method == "POST":
+        m = sql.connect(host="localhost", user="root", passwd="kshitij2803", database="sports")
+        cursor = m.cursor()
+        d = request.POST
+        for key, value in d.items():
+            if key == "email":
+                em = value
+            if key == "password":
+                pwd = value
+
+        c = "select * from users where email='{}' and password='{}'".format(em, pwd)
         cursor.execute(c)
         t = tuple(cursor.fetchall())
         if t != ():
