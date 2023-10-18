7e113f03ca529963313d1f9d25073190f26d44c1
JugalKothari <jugalprakashk19@gmail.com>
2023-10-18 09:40:32 +0530

    Update sports.sql
    Added users table

--- sports.sql
+++ sports.sql
@@ -258,12 +258,31 @@ LOCK TABLES `trainer` WRITE;
 UNLOCK TABLES;
 
 --
--- Dumping events for database 'sports'
+-- Table structure for table `users`
 --
 
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `First_name` varchar(20) NOT NULL,
+  `Last_name` varchar(20) NOT NULL,
+  `Sex` enum('M','F') NOT NULL,
+  `Email` varchar(30) NOT NULL,
+  `Password` varchar(20) NOT NULL,
+  UNIQUE KEY `Email` (`Email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
 --
--- Dumping routines for database 'sports'
+-- Dumping data for table `users`
 --
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES ('Jugal','Kothari','M','jugalprakashk19@gmail.com','jugal123'),('Kshitij','Agarwal','M','kshitijagarwal28@gmail.com','kshitij123');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -274,4 +293,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-10-17 14:13:27
+-- Dump completed on 2023-10-18  9:38:07
