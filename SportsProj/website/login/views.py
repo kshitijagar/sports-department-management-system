36bf3821e8e8297a0e9244bc72cef583b12ac7ef
JugalKothari <jugalprakashk19@gmail.com>
2023-10-19 08:46:24 +0530

    equipments and courts

--- SportsProj/website/login/views.py
+++ SportsProj/website/login/views.py
@@ -17,6 +17,7 @@ def loginaction(request):
 
         c = "select * from users where email='{}' and password='{}'".format(em, pwd)
         cursor.execute(c)
+<<<<<<< Updated upstream
         t = tuple(cursor.fetchall())
         if t != ():
             return render(request, "welcome.html")
@@ -24,3 +25,11 @@ def loginaction(request):
             error_message = "Invalid username or password."
 
     return render(request, 'login_page.html', {'error_message': error_message})
+=======
+        t=tuple(cursor.fetchall())
+        if t==():
+            return render(request,'error.html')
+        else:
+            return render(request, "home.html")
+    return render(request, 'login_page.html')
+>>>>>>> Stashed changes
--- SportsProj/website/website/settings.py
+++ SportsProj/website/website/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'signup',
-    'login'
+    'login',
+    'equipments',
+    'courts'
 ]
 
 MIDDLEWARE = [
