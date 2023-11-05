20051a23f667a2975c0a5e3814d1d5bb76866191
kshitijagar <kshitijagarwal2808@gmail.com>
2023-11-06 01:12:31 +0530

    SRN forwarding

--- SportsProj/website/login/views.py
+++ SportsProj/website/login/views.py
@@ -1,3 +1,34 @@
+# from django.shortcuts import render, redirect
+# import mysql.connector as sql
+
+# def loginaction(request):
+#     global em, pwd
+#     error_message = ''  # Initialize error_message here
+
+#     if request.method == "POST":
+#         m = sql.connect(host="localhost", user="root", passwd="kshitij2803", database="sports")
+#         cursor = m.cursor()
+#         d = request.POST
+#         for key, value in d.items():
+#             if key == "email":
+#                 em = value
+#             if key == "password":
+#                 pwd = value
+
+#         c = "select * from student where emailid='{}' and password='{}'".format(em, pwd)
+#         cursor.execute(c)
+        
+#         t = tuple(cursor.fetchall())
+#         d = "select * from student where SRN='{}' and password='{}'".format(em, pwd)
+#         cursor.execute(d)
+#         k= tuple(cursor.fetchall())
+#         if t != () or k!= () :
+#             return redirect("http://localhost:8000/home/")
+#         else:
+#             error_message = "Invalid username or password."
+
+#     return render(request, 'login_page.html', {'error_message': error_message})
+
 from django.shortcuts import render, redirect
 import mysql.connector as sql
 
@@ -21,9 +52,19 @@ def loginaction(request):
         t = tuple(cursor.fetchall())
         d = "select * from student where SRN='{}' and password='{}'".format(em, pwd)
         cursor.execute(d)
-        k= tuple(cursor.fetchall())
-        if t != () or k!= () :
-            return redirect("http://localhost:8000/home/")
+        
+        k = tuple(cursor.fetchall())
+        
+        if t != () or k != ():
+            if t:
+                u="select SRN from student where emailid='{}'".format(em)
+                cursor.execute(u)
+                SRN= str(cursor.fetchall()[0])
+                SRN=SRN[2:-3]
+                print(SRN)
+                return redirect("http://localhost:8000/home/?SRN={}".format(SRN))
+            else:
+                return redirect("http://localhost:8000/home/?SRN={}".format(em))
         else:
             error_message = "Invalid username or password."
 
