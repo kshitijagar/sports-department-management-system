1ba1581cbca6eb689a5c934f50f0bcc43cdbbc62
JugalKothari <jugalprakashk19@gmail.com>
2023-11-09 09:16:30 +0530

    final thursday morning

--- SportsProj/website/courts/views.py
+++ SportsProj/website/courts/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 def courtaction(request):
     srn=request.GET.get('SRN')
-    return render(request,'courts.html')
+    return render(request, 'courts.html',{'SRN': srn})
