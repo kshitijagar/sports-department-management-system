335435652f9c352ccb9e5cb0da3e4c9902c67541
kshitijagar <kshitijagarwal2808@gmail.com>
2023-11-20 09:52:47 +0530

    proc working

--- SportsProj/website/achievements/views.py
+++ SportsProj/website/achievements/views.py
@@ -9,25 +9,29 @@ def achieveaction(request):
     #c = "SELECT s.name, a.position, a.awardorg, a.cashprize FROM student s JOIN achievements a ON s.SRN = a.SRN WHERE s.SRN = '{}' ORDER BY a.cashprize DESC;".format(srn)
     cursor.callproc( 'GetStudentAchievements', [srn,])
     #cursor.execute()
+    t=[]
+    for i in cursor.stored_results():
+        t.append(i.fetchall())
 
-    t = tuple(cursor.fetchall())
+    print(t)
     student_name = ''
     achievements = []
 
-    for i in t:
-        a = str(i)
-        a = a[1:-1]
-        s = a.split(", ")
-        student_name = s[0][1:-1]
-        achievements.append({
-            'position': s[1],
-            'awardorg': s[2][1:-1],
-            'cashprize': s[3]
-        })
+    for j in t:
+        for i in j:
+            a = str(i)
+            a = a[1:-1]
+            s = a.split(", ")
+            student_name = s[0][1:-1]
+            achievements.append({
+                'position': s[1],
+                'awardorg': s[2][1:-1],
+                'cashprize': s[3]
+            })
     cur = m.cursor()
     d = "SELECT CalculateTotalCashPrize('{}')".format(srn)
     cur.execute(d)
-    print(t)
+    
     k = tuple(cursor.fetchone())
     print(k)
     
